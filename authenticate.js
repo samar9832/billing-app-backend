4aeb964b97f9e67c5eb2ee3e82ee4e7842aa9181
samar9832 <hritikpu123@gmail.com>
2025-04-15 13:07:24 +0530

    Create authenticate.js

---
+++ authenticate.js
@@ -0,0 +1,14 @@
+const jwt = require('jsonwebtoken');
+
+module.exports = function (req, res, next) {
+  const authHeader = req.headers.authorization;
+  const token = authHeader && authHeader.split(' ')[1];
+
+  if (!token) return res.sendStatus(401);
+
+  jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
+    if (err) return res.sendStatus(403);
+    req.user = decoded;
+    next();
+  });
+};
